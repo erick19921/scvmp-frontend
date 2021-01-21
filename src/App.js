f811058147e1283000e4a52fe83c24ae8137e938
Miguel Rodríguez <miguelrodrii@outlook.com>
2021-01-21 09:47:07 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_PRODUCTOS = gql`
+{
+  productos {
+    id,
+    costo_compra_no_iva,
+    costo_venta_no_iva,
+    cantidad_disponible,
+    fecha_expiracion
+  }
+}
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(GET_PRODUCTOS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <div class="table">
+          <div class="table-contenido">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Costo compra sin iva</th>
+                  <th>Costo venta sin iva</th>
+                  <th>Cantidad disponible</th>
+                  <th>Fecha expiracion</th>
+                  <th>Opciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.productos.map(({
+                  id,
+                  costo_compra_no_iva,
+                  costo_venta_no_iva,
+                  cantidad_disponible,
+                  fecha_expiracion
+                }) => (
+                  <tr key={id}>
+                    <td>
+                      {id}
+                    </td>
+                    <td>
+                      {costo_compra_no_iva}
+                    </td>
+                    <td>
+                      {costo_venta_no_iva}
+                    </td>
+                    <td>
+                      {cantidad_disponible}
+                    </td>
+                    <td>
+                      {fecha_expiracion}
+                    </td>
+                    <td>
+                      <button onClick="" data-id={id} type="button" class="btn btn-outline-secondary"><i class="fas fa-pencil-alt"></i></button>
+                      <button data-id={id} type="button" class="btn btn-outline-secondary"><i class="fas fa-trash"></i></button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+//JS
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +23,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
